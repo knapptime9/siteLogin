722cbaf353637cbabaf7e0a10074d5f92b10f4ae
knapptime9 <knappj84@yahoo.com>
2017-08-23 11:11:44 -0400

    fully working submission Hooray

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = express();
 
-// ========== Define templates ==========
-app.engine('mustache-express', expressMustache());
+app.engine('mustache', expressMustache());
 app.set('views', './views');
-app.set('view engine', 'mustache-express');
+app.set('view engine', 'mustache');
 
 // ========== code for express-session ==========
+//for body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.use(
   session({
@@ -22,29 +26,38 @@ app.use(
 );
 
 
-//for body-parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+
+// app.get('/',function(req, res) {
+//   res.render('index');
+// });
 
 
-// path to index
-app.get('./views', function(req, res) {
-  // res.sender("Hello!!!!!!"); displays "Hello!!!!!!" on the / page.
-  if (!req.session.loginID) {
-    res.redirect('login')
+
+  // path to index
+  app.get('/', function(req, res) {
+  if (!req.session.authorized) {
+    res.redirect('/login')
   } else {
-    res.render('index', {
-      username: req.session.loginID
-    });
+    res.render('index'), {
+    };
   }
 });
-
 // path to login
-app.get('./views', function(req, res) {
+app.get('/login', function(req, res) {
   res.render('login')
 });
+
+app.post('/login', function(req, res){
+  var loginID = req.body.loginID
+  req.session.loginID = loginID
+  req.session.authorized = true
+  res.redirect('/')
+  // console.log(req.session)
+  // console.log(req.body.loginID)
+  // console.log(req.session.loginID)
+  // console.log(loginID)
+})
+
 
 
 // ========== make express listen on port 3000 ==========
